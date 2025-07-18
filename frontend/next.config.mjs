79392d2c6d9200fa185016b51708cf5fe35392d2
Yoru Akio <yoruakio@proton.me>
2025-07-18 23:20:28 +0700

    feat: update Next.js configuration with output, distDir, compress, and image settings

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+
+  distDir: 'dist',
+  compress: false,
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
